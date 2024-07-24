f093c67165634d4a76576b1f1bb6b4971fc55d14
Jx Li <152577071+skill-diver@users.noreply.github.com>
2024-07-24 01:40:38 -0400

    Update radio_model.py

--- radio/radio_model.py
+++ radio/radio_model.py
@@ -206,6 +206,55 @@ class RADIOModel(nn.Module):
         return radio_outputs
 
 
+# def create_model_from_args(args) -> nn.Module:
+#     in_chans = 3
+#     if args.in_chans is not None:
+#         in_chans = args.in_chans
+#     elif args.input_size is not None:
+#         in_chans = args.input_size[0]
+
+#     # Skip weight initialization unless it's explicitly requested.
+#     weight_init = args.model_kwargs.pop("weight_init", "skip")
+
+#     model = create_model(
+#         args.model,
+#         pretrained=args.pretrained,
+#         in_chans=in_chans,
+#         num_classes=args.num_classes,
+#         drop_rate=args.drop,
+#         drop_path_rate=args.drop_path,
+#         drop_block_rate=args.drop_block,
+#         global_pool=args.gp,
+#         bn_momentum=args.bn_momentum,
+#         bn_eps=args.bn_eps,
+#         scriptable=args.torchscript,
+#         checkpoint_path=args.initial_checkpoint,
+#         weight_init=weight_init,
+#         **args.model_kwargs,
+#     )
+
+#     if hasattr(model, 'norm') and not getattr(args, 'model_norm', False):
+#         model.norm = nn.Identity()
+
+#     model.head = nn.Identity()
+
+#     assert (
+#         not args.cls_token_per_teacher or args.cpe_max_size is not None
+#     ), "CPE must be enabled for multiple CLS tokens!"
+
+#     if args.cpe_max_size is not None:
+#         uq_teachers = set(t['name'] for t in args.teachers)
+#         enable_cpe(
+#             model,
+#             args.cpe_max_size,
+#             num_cls_tokens=len(uq_teachers) if args.cls_token_per_teacher else 1,
+#             register_multiple=args.register_multiple,
+#         )
+
+#     if args.spectral_reparam:
+#         configure_spectral_reparam_from_args(model, args)
+
+#     return model
 def create_model_from_args(args) -> nn.Module:
     in_chans = 3
     if args.in_chans is not None:
@@ -251,6 +300,10 @@ def create_model_from_args(args) -> nn.Module:
             register_multiple=args.register_multiple,
         )
 
+    # default process
+    if not hasattr(args, 'spectral_reparam'):
+        args.spectral_reparam = False
+    
     if args.spectral_reparam:
         configure_spectral_reparam_from_args(model, args)
 
